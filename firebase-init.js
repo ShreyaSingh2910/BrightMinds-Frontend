e166628ef00120ddeeae7db57af107dfdb36ed2e
ShreyaSingh2910 <shreyapcm20@gmail.com>
2026-02-11 09:01:45 +0530

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBmz23pK8TQ8iE5_EjRbOo0qCazLOBmcBw",
+  authDomain: "brightminds-52de2.firebaseapp.com",
+  projectId: "brightminds-52de2",
+  storageBucket: "brightminds-52de2.appspot.com",
+  messagingSenderId: "855958185432",
+  appId: "1:855958185432:web:bf52544dc223ffd6f4fead"
+};
+
+if(!firebase.apps.length)
+firebase.initializeApp(firebaseConfig);
